04b9af47a8671b46edf67add95f83b66e08bbd26
dyiar <austin.hl.blake@gmail.com>
2019-01-18 10:40:24 -0800

    finished CRUD operations for actions and projects.

--- actionsRoutes/actionsRoutes.js
+++ actionsRoutes/actionsRoutes.js
@@ -5,19 +5,74 @@ const router = express.Router();
 
 //middleware
 
+function checkIdExists(req, res, next) {
+  const id = req.params.id;
+
+  db.get(id).then(id => {
+    if (id) {
+      next();
+    } else {
+      res.status(404).end({ error: "The ID doesn't exist." });
+    }
+  });
+}
+
 //endpoints
 
 router.post("/", (req, res) => {
-  db("actions")
-    .insert(req.body)
-    .then(ids => {
-      db("actions")
-        .where({ id: ids[0] })
-        .then(newAction => {
-          res.status(201).send({ newAction });
-        });
+  db.insert(req.body)
+    .then(result => {
+      db.get(result.id).then(newAction => {
+        res.status(201).send({ newAction });
+      });
     })
     .catch(() => res.status(500).send({ error: "Data couldn't be saved." }));
+});
+
+router.get("/", (req, res) => {
+  db.get()
+    .then(actions => {
+      res.status(200).send({ actions });
+    })
+    .catch(() =>
+      res.status(500).send({ error: "The data could not be retrieved." })
+    );
+});
+
+router.get("/:id", checkIdExists, (req, res) => {
+  const id = req.params.id;
+  db.get(id)
+    .then(action => {
+      res.status(200).send({ action });
+    })
+    .catch(() =>
+      res.status(500).send({ error: "The actions could not be retrieved." })
+    );
+});
+
+router.put("/:id", checkIdExists, (req, res) => {
+  const id = req.params.id;
+  const change = req.body;
+
+  db.update(id, change)
+    .then(result => {
+      res.status(200).send({ result });
+    })
+    .catch(() =>
+      res.status(500).send({ error: "The data could not be saved." })
+    );
+});
+
+router.delete("/:id", checkIdExists, (req, res) => {
+  const id = req.params.id;
+
+  db.remove(id)
+    .then(count => {
+      res.status(200).send({ deleted: count });
+    })
+    .catch(() =>
+      res.status(500).send({ error: "The data could not be deleted." })
+    );
 });
 
 module.exports = router;
--- projectsRoutes/projectsRoutes.js
+++ projectsRoutes/projectsRoutes.js
@@ -6,16 +6,14 @@ const router = express.Router();
 // middleware
 
 function checkIdExists(req, res, next) {
-    const id = req.params.id
-  db
-    .get(id)
-    .then(id => {
-      if (id) {
-        next();
-      } else {
-        res.status(404).send({ error: "The ID doesn't exist." });
-      }
-    });
+  const id = req.params.id;
+  db.get(id).then(id => {
+    if (id) {
+      next();
+    } else {
+      res.status(404).send({ error: "The ID doesn't exist." });
+    }
+  });
 }
 
 //endpoints
@@ -40,5 +38,35 @@ router.get("/:id", checkIdExists, (req, res) => {
       res.status(500).send({ error: "The data could not be retrieved." });
     });
 });
+
+router.get("/", (req, res) => {
+  db.get()
+    .then(projects => {
+      res.status(200).send({ projects });
+    })
+    .catch(() => {
+      res.send(500).send({ error: "The data could not be retrieved." });
+    });
+});
+
+router.put("/:id", checkIdExists, (req, res) => {
+  const id = req.params.id;
+  change = req.body;
+
+  db.update(id, change)
+    .then(result => res.status(200).send({ result }))
+    .catch(() =>
+      res.status(500).send({ error: "The data could not be updated." })
+    );
+});
+
+router.delete('/:id', checkIdExists, (req, res) => {
+    const id = req.params.id
+
+    db.remove(id).then(count => {
+        res.status(200).send({ count })
+    })
+    .catch(() => res.status(500).send({ error: "The data could not be deleted." }))
+})
 
 module.exports = router;
